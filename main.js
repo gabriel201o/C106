f6863b4c58f10394358bedd1b5e70870fe596f9e
gabriel201o <80070076+gabriel201o@users.noreply.github.com>
2021-07-29 18:59:58 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+prediction_1="";
+prediction_2="";
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera')
+
+
